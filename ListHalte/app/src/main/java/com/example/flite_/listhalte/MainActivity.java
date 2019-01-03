547089642ae8e6b35a3ca0b61c468e7522ce3bc8
flite- <ariefpambudi98@gmail.com>
2019-01-03 17:14:16 +0700

    New List Halte

--- ListHalte/app/src/main/java/com/example/flite_/listhalte/MainActivity.java
+++ ListHalte/app/src/main/java/com/example/flite_/listhalte/MainActivity.java
@@ -1,17 +1,15 @@
-package com.example.flite_.halte;
+package com.example.flite_.listhalte;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.ListAdapter;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     ListView listView;
 
     @Override
@@ -20,25 +18,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         listView = (ListView)findViewById(R.id.list1);
 
-        String[] values = new String[]{"button1", "button2", "button3"};
+        String[] values = new String[]{
+                "Halte 1",
+                "Halte 2",
+                "Halte 3"};
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.activity_list_item,android.R.id.text1,values);
         listView.setAdapter(adapter);
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id){
                 if(position==0){
-                    Intent myintent = new Intent(view.getContext(),Main2Activity.class);
-                    startActivityForResult(myintent,0);
-                }
-                if(position==1){
-                    Intent myintent = new Intent(view.getContext(),Main2Activity.class);
+                    Intent myintent = new Intent(view.getContext(),halte1.class);
                     startActivityForResult(myintent,1);
                 }
-                if(position==2){
-                    Intent myintent = new Intent(view.getContext(),Main2Activity.class);
+                if(position==1){
+                    Intent myintent = new Intent(view.getContext(),halte2.class);
                     startActivityForResult(myintent,2);
+                }
+                if(position==2){
+                    Intent myintent = new Intent(view.getContext(),halte3.class);
+                    startActivityForResult(myintent,3);
                 }
             }
         });
+
     }
 }
